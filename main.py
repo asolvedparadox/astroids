338c5284f0973934852706bad142da72c9a25c5a
asolvedparadox <asolvedparadox@gmail.com>
2026-03-18 20:38:17 -0500

    CH3-L3 Moving around - update from solution

--- main.py
+++ main.py
@@ -7,22 +7,24 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-    dt = 0
-
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
         player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
-        pygame.display.flip()  
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS  
         dt = clock.tick(60) / 1000
 
 
--- player.py
+++ player.py
@@ -8,8 +8,9 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "White", self.triangle(), LINE_WIDTH)
 
-    # in the Player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -18,14 +19,6 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-    def draw(self, screen):
-        pygame.draw.polygon(screen, "White", self.triangle(), LINE_WIDTH)
-
-
-    def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
-
-
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -33,3 +26,6 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
