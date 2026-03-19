5b4f84d1ad0309936f07badc3f3342c285f8c062
asolvedparadox <asolvedparadox@gmail.com>
2026-03-18 20:32:19 -0500

    CH3 - L3 Moving Around

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()  
-        pygame.time.Clock().tick(60)
-        dt = pygame.time.Clock().tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
